587f60c80548ad0a4142d731e12df5aedc1e96e4
franciscoDastres <franciscomeza.1234@gmail.com>
2025-10-17 21:10:00 -0300

    feat: :sparkles: modifing for lua for clean code
    adding comentaries in exercise without using for

---
+++ for.lua
@@ -0,0 +1,17 @@
+--print("ingrese un numero ")
+--local entrada = io.read("*n") -- lee un número desde la consola
+
+--print("el numero ingresado es ", entrada + 1)
+
+
+if (entrada >= 21) then
+    print("puedes acceder al siguiente nivel")
+else
+    print(" aun te falta mi pequeño tilin")
+end
+
+
+for i = 10, 19, 1
+do
+    print(i)
+end
